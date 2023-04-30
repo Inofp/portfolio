9a2a1491d6a0672a504d1aa766f63c7115b21cdc
arimafp <arimafp@mail.ru>
2023-05-01 02:21:26 +0300

    upd mobile responsive

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
         <div className='flex justify-center min-h-full'>
             <div className="max-w-[1420px] my-10">
                 <div className='w-full flex flex-col lg:flex-row justify-center  max-lg:items-center'>
-                    <div className="min-w-[300px] order-2 lg:order-1 max-lg:pb-5">
+                    <div className="min-w-[300px] order-2 lg:order-1 max-lg:pb-5 max-lg:px-1">
                     <SideNavBar />
                     </div>
                     <div className='lg:order-2 w-full '>
