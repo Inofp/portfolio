689ed85e7f707f2ec1de3266c6911db48ae200f1
arimafp <ari_khvalko@mail.ru>
2024-03-22 02:41:14 +0300

    add yandex metrika

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const yandexMetrikaId = process.env.YANDEX_METRIKA_ID;
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,6 +9,28 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(${yandexMetrikaId}, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src={`https://mc.yandex.ru/watch/${yandexMetrikaId}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+
       </body>
     </Html>
   )
